8f42b9a94f13fa8b6cbd6e30751f73945569ae1a
qibal <iqbalherlambang.39@gmail.com>
2024-10-15 16:09:17 +0700

    pratikum ke 2

---
+++ Praktikum/P2_1_714230061/P2_1_714230061/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan Angka Pertama : ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masukkan Angka Kedua : ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Angka Pertama = " + input);
+            Console.WriteLine("Angka Kedua = " + input2);
+            Console.WriteLine(input + " + " + input2 + " = " + (input + input2));
+            Console.WriteLine(input + " - " + input2 + " = " + (input - input2));
+            Console.WriteLine(input + " x " + input2 + " = " + (input * input2));
+            Console.WriteLine(input + " / " + input2 + " = " + (input / input2));
+
+        }
+    }
+}
